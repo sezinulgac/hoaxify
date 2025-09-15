b13c78a1140e20a896bf51e1d6d1e3405df2184a
sezinulgac <sezinulgac2@gmail.com>
2025-09-15 21:25:17 +0300

    added to react account activation request

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 //import App from './App.jsx'
 import { RouterProvider } from "react-router-dom";
@@ -7,7 +6,5 @@ import "./styles.scss";
 import router from "./router/index.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
 );
--- frontend/src/pages/Activation/index.jsx
+++ frontend/src/pages/Activation/index.jsx
@@ -1,6 +1,38 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { activateUser } from "./api";
 
 export function Activation() {
-    const {token} = useParams()
-    return <div>Activation token :{token}</div>
+  const { token } = useParams();
+  const [apiProgress, setApiProgress] = useState();
+    const [successMessage, setSuccessMessage] = useState();
+    const [errorMessage, setErrorMessage] = useState();
+    
+  useEffect(() => {
+    async function activate() {
+        setApiProgress(true);
+      try {
+        const response = await activateUser(token);
+        setSuccessMessage(response.data.message)
+      } catch (axiosError) { 
+        setErrorMessage(axiosError.response.data.message)
+      }
+      finally{
+        setApiProgress(false);
+      }
+    }
+    activate();
+  }, []);
+
+  return (
+    <>
+    {apiProgress && (<span
+                  className="spinner-border "
+                  aria-hidden="true"
+                ></span>)}
+    {successMessage && (<div className="alert alert-success">{successMessage}</div>)}
+    {errorMessage && (<div className="alert alert-danger">{errorMessage}</div>)}
+
+    </>
+  );
 }
