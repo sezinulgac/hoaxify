8139212f9c7c1513e2ec2f9d7be7b99fca41385b
sezinulgac <sezinulgac2@gmail.com>
2025-09-16 14:38:34 +0300

    added 25 users with main application

--- webservice/src/main/java/com/hoaxify/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/hoaxify/webservice/WebserviceApplication.java
@@ -1,14 +1,40 @@
 package com.hoaxify.webservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.hoaxify.webservice.user.User;
+import com.hoaxify.webservice.user.UserRepository;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("dev")
+	CommandLineRunner userCreator(UserRepository userRepository) {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return (args) -> {
+			for (var i = 1; i <= 25; i++) {
+				User user = new User();
+				user.setUsername("user" + i);
+				user.setEmail("user" + i + "@mail.com");
+				user.setPassword(encoder.encode("P4ssword"));
+				user.setActive(true);
+				userRepository.save(user);
+			}
+ 
+		};
+
 	}
 
 }
