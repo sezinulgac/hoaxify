07d60d409fc527c493ebc087bfb26d73923b1974
sezinulgac <sezinulgac2@gmail.com>
2025-09-18 16:21:51 +0300

    fixed key errors

--- frontend/src/pages/Home/components/UserListItem.jsx
+++ frontend/src/pages/Home/components/UserListItem.jsx
@@ -1,13 +1,6 @@
-import { useState } from "react";
-
 export function UserListItem({ user }) {
-  const [selected, setSelected] = useState(false);
 
   return (
-    <li
-      className={`list-group-item list-group-item-action ${selected ? 'active':''}`}
-      onClick={() => setSelected(!selected)}>
-      {user.username}
-    </li>
+    <li className="list-group-item list-group-item-action" > {user.username} </li>
   );
 }
